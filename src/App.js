97b64b710bc05c8945b109610fe1cba1057108e5
Rohan Sharma <onlyrohan21@gmail.com>
2023-07-12 16:51:01 +0530

    t2s functionality

--- src/App.js
+++ src/App.js
@@ -3,13 +3,25 @@ import SpeechRecognition, {
   useSpeechRecognition,
 } from "react-speech-recognition";
 import useClipboard from "react-use-clipboard";
-import { useState } from "react";
+import React, { useState } from "react";
+// import { Container, Segment } from "semantic-ui-react";
+import "./App.css";
+import { useSpeechSynthesis } from "react-speech-kit";
+import { isDisabled } from "@testing-library/user-event/dist/utils";
 
 const App = () => {
   const [textToCopy, setTextToCopy] = useState();
+
   const [isCopied, setCopied] = useClipboard(textToCopy, {
     successDuration: 1000,
   });
+  //T2S
+  const [text, setText] = useState("");
+  const { speak } = useSpeechSynthesis();
+
+  const handleOnClick = () => {
+    speak({ text: text });
+  };
 
   const startListening = () =>
     SpeechRecognition.startListening({ continuous: true, language: "en-IN" });
@@ -23,17 +35,31 @@ const App = () => {
   return (
     <>
       <div className="container">
-        <h2>Speech to Text Converter</h2>
+        <h2>
+          Speech-2-Text | Text-2-Speech <br />
+          Converter
+        </h2>
         <br />
         <p>
           Continous Sentiment Analysis using Natural Language Processing to be
           implemented soon..
         </p>
+        <div className="main-content">
+          <div
+            className="min-container"
+            onClick={() => setTextToCopy(transcript)}
+          >
+            {transcript}
+          </div>
 
-        <div className="main-content" onClick={() => setTextToCopy(transcript)}>
-          {transcript}
+          <textarea
+            className="min-container"
+            onChange={(e) => {
+              setText(e.target.value);
+            }}
+            placeholder="Write anything?"
+          ></textarea>
         </div>
-
         <div className="btn-style">
           <button onClick={setCopied}>
             {isCopied ? "Copied!" : "Copy to clipboard"}
@@ -41,6 +67,13 @@ const App = () => {
           <button onClick={startListening}>Start Listening</button>
           <button onClick={SpeechRecognition.stopListening}>
             Stop Listening
+          </button>
+          <button
+            onClick={() => {
+              handleOnClick();
+            }}
+          >
+            Text-2-Speech
           </button>
         </div>
       </div>
